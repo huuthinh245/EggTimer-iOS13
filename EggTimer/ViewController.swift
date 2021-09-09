344e122366c7d9c83ec6e111242c8504018438cb
thinh <thinh@thinhs-MacBook-Pro.local>
2021-09-09 11:52:17 +0700

    init

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var progress: UIProgressView!
+    @IBOutlet weak var lblEgg: UILabel!
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    var count = 0;
+    var total: Int = 0
+    var timer: Timer?
+    @IBAction func handlePress(sender: UIButton) {
+        timer?.invalidate()
+        let title: String = sender.currentTitle!
+        total = eggTimes[title]!
+        lblEgg.text = title
+        progress.progress = 0.0
+        count = 0
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(UIMenuController.update), userInfo: nil, repeats: true)
+    }
     
-
-
+    @objc func update() {
+        if count < total {
+            print("\(count) s");
+                        count += 1
+            let percent = Float(count) / Float(total)
+            progress.progress = percent
+        }else {
+            timer?.invalidate()
+            lblEgg.text = "Done"
+        }
+    }
 }
